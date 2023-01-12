995549c3c7abd4b6501d64f535cd238734435bbc
sug896 <122576599+sug896@users.noreply.github.com>
2023-01-12 18:52:47 -0500

    Update Tree.java
    added variables and created constructor

--- Tree.java
+++ Tree.java
@@ -1,1 +1,10 @@
 
+class Tree {
+    double heightFt;
+    double trunkDiameterInches;
+
+    public Tree(double heightFt, double trunkDiameterInches) {
+        this.heightFt = heightFt;
+        this.trunkDiameterInches = trunkDiameterInches;
+    }
+}
